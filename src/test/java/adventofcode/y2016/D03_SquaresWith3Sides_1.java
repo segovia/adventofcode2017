f8b8ce62c6a6bea2f7cfb92f5bd933e0203e6f94
Gustavo Segovia <gustavo.segovia@gmail.com>
2017-12-09 21:46:57 +0100

    D03 2016

---
+++ src/test/java/adventofcode/y2016/D03_SquaresWith3Sides_1.java
@@ -0,0 +1,35 @@
+package adventofcode.y2016;
+
+import adventofcode.Utils;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class D03_SquaresWith3Sides_1 {
+
+    @Test
+    public void test() throws IOException {
+        assertThat(run("5 10 25\n10 10 10\n"), is(1));
+        List<String> fileInputs = Utils.getInputsFromFiles(D03_SquaresWith3Sides_1.class);
+        assertThat(run(fileInputs.get(0)), is(993));
+    }
+
+    private int run(String input) {
+        String[] lines = input.split("\\n");
+        int count = 0;
+        for (String line : lines) {
+            int[] sides = Arrays.stream(line.trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+            Arrays.sort(sides);
+            if (sides[0] + sides[1] > sides[2]) {
+                ++count;
+            }
+        }
+        return count;
+
+    }
+}
---
+++ src/test/java/adventofcode/y2016/D03_SquaresWith3Sides_2.java
@@ -0,0 +1,48 @@
+package adventofcode.y2016;
+
+import adventofcode.Utils;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class D03_SquaresWith3Sides_2 {
+
+    @Test
+    public void test() throws IOException {
+        assertThat(run("5 10 25\n10 10 10\n3 10 30\n"), is(2));
+        List<String> fileInputs = Utils.getInputsFromFiles(D03_SquaresWith3Sides_2.class);
+        assertThat(run(fileInputs.get(0)), is(993));
+    }
+
+    private int run(String input) {
+        String[] lines = input.split("\\n");
+        int count = 0;
+        for (int i = 0; i < lines.length; i += 3) {
+            int[] row1 = readRow(lines[i]);
+            int[] row2 = readRow(lines[i + 1]);
+            int[] row3 = readRow(lines[i + 2]);
+
+            for (int j = 0; j < 3; j++) {
+                if (isTriangle(new int[]{row1[j], row2[j], row3[j]})) {
+                    ++count;
+                }
+            }
+        }
+        return count;
+
+    }
+
+    private boolean isTriangle(int[] sides) {
+        Arrays.sort(sides);
+        return sides[0] + sides[1] > sides[2];
+    }
+
+    private int[] readRow(String line) {
+        return Arrays.stream(line.trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+    }
+}
