72e4430680ec43dd68e138b9ea10cfa48ecc2718
Gustavo Segovia <gustavo.segovia@gmail.com>
2017-12-09 21:27:17 +0100

    D02 2016

---
+++ src/test/java/adventofcode/y2016/D02_BathroomSecurity_1.java
@@ -0,0 +1,51 @@
+package adventofcode.y2016;
+
+import adventofcode.Utils;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class D02_BathroomSecurity_1 {
+
+    @Test
+    public void test() throws IOException {
+        List<String> fileInputs = Utils.getInputsFromFiles(D02_BathroomSecurity_1.class);
+        assertThat(run(fileInputs.get(0)), is("1985"));
+        assertThat(run(fileInputs.get(1)), is("44558"));
+    }
+
+    private String run(String input) {
+        int i = 1;
+        int j = 1;
+
+        StringBuilder code = new StringBuilder();
+        String[] lines = input.split("\\n");
+        for (String line : lines) {
+            for (int k = 0; k < line.length(); k++) {
+                char c = line.charAt(k);
+                switch (c) {
+                    case 'U':
+                        i -= (i > 0) ? 1 : 0;
+                        break;
+                    case 'D':
+                        i += (i < 2) ? 1 : 0;
+                        break;
+                    case 'L':
+                        j -= (j > 0) ? 1 : 0;
+                        break;
+                    case 'R':
+                        j += (j < 2) ? 1 : 0;
+                        break;
+                }
+            }
+
+            code.append(i * 3 + j + 1);
+        }
+        return code.toString();
+
+    }
+}
---
+++ src/test/java/adventofcode/y2016/D02_BathroomSecurity_2.java
@@ -0,0 +1,59 @@
+package adventofcode.y2016;
+
+import adventofcode.Utils;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class D02_BathroomSecurity_2 {
+
+    char[][] keyPad = new char[][]{
+            new char[]{'X', 'X', '1', 'X', 'X'},
+            new char[]{'X', '2', '3', '4', 'X'},
+            new char[]{'5', '6', '7', '8', '9'},
+            new char[]{'X', 'A', 'B', 'C', 'X'},
+            new char[]{'X', 'X', 'D', 'X', 'X'},
+    };
+
+    @Test
+    public void test() throws IOException {
+        List<String> fileInputs = Utils.getInputsFromFiles(D02_BathroomSecurity_2.class);
+        assertThat(run(fileInputs.get(0)), is("5DB3"));
+        assertThat(run(fileInputs.get(1)), is("6BBAD"));
+    }
+
+    private String run(String input) {
+        int i = 2;
+        int j = 0;
+
+        StringBuilder code = new StringBuilder();
+        String[] lines = input.split("\\n");
+        for (String line : lines) {
+            for (int k = 0; k < line.length(); k++) {
+                char c = line.charAt(k);
+                switch (c) {
+                    case 'U':
+                        i -= (i > 0 && keyPad[i - 1][j] != 'X') ? 1 : 0;
+                        break;
+                    case 'D':
+                        i += (i < 4 && keyPad[i + 1][j] != 'X') ? 1 : 0;
+                        break;
+                    case 'L':
+                        j -= (j > 0 && keyPad[i][j - 1] != 'X') ? 1 : 0;
+                        break;
+                    case 'R':
+                        j += (j < 4 && keyPad[i][j + 1] != 'X') ? 1 : 0;
+                        break;
+                }
+            }
+
+            code.append(keyPad[i][j]);
+        }
+        return code.toString();
+
+    }
+}
