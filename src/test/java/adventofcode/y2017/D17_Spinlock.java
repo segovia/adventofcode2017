1441ab78352544ae58deb9f7594019a9c468bded
Gustavo Segovia <gustavo.segovia@gmail.com>
2017-12-17 11:48:01 +0100

    D17 2017

---
+++ src/test/java/adventofcode/y2017/D17_Spinlock.java
@@ -0,0 +1,48 @@
+package adventofcode.y2017;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class D17_Spinlock {
+
+    @Test
+    public void test() throws IOException {
+        assertThat(run(3), is(638));
+        assertThat(run(329), is(725));
+    }
+
+    private int run(int steps) {
+        CircularListNode cur = new CircularListNode(0);;
+        for (int i = 1; i <= 2017; i++) {
+            for (int j = 0; j < steps; j++) {
+                cur = cur.next;
+            }
+            CircularListNode.newAfter(cur, i);
+            cur = cur.next;
+        }
+        return cur.next.val;
+    }
+
+    static class CircularListNode {
+        int val;
+        CircularListNode next;
+
+        CircularListNode(int val) {
+            this.val = val;
+            next = this;
+        }
+
+        static CircularListNode newAfter(CircularListNode n, int val) {
+            CircularListNode after = new CircularListNode(val);
+            CircularListNode aux = n.next;
+            n.next = after;
+            after.next = aux;
+            return after;
+        }
+    }
+
+}
---
+++ src/test/java/adventofcode/y2017/D17_Spinlock_2.java
@@ -0,0 +1,30 @@
+package adventofcode.y2017;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class D17_Spinlock_2 {
+
+    @Test
+    public void test() throws IOException {
+        assertThat(run(3), is(1222153));
+        assertThat(run(329), is(27361412));
+    }
+
+    private int run(int steps) {
+        int cur = 0;
+        int valAfterZero = -1;
+        for (int len = 1; len <= 50_000_000; len++) {
+            int next = (cur + steps) % len;
+            if (next == 0) valAfterZero = len;
+            cur = (next + 1) % (len + 1);
+        }
+        return valAfterZero;
+    }
+
+
+}
