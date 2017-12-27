b997f22de61b0b84074b980f5f5684d3efdabbac
Gustavo Segovia <gustavo.segovia@gmail.com>
2017-12-28 00:27:26 +0100

    D14 2016 - small refactor

--- src/test/java/adventofcode/y2016/D14_OneTimePad.java
+++ src/test/java/adventofcode/y2016/D14_OneTimePad.java
@@ -1,11 +1,11 @@
 package adventofcode.y2016;
 
+import adventofcode.Utils;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.*;
 
-import static adventofcode.Utils.doMD5;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -35,40 +35,40 @@ public class D14_OneTimePad {
 
         PriorityQueue<Integer> keys = new PriorityQueue<>(Comparator.reverseOrder());
         for (int i = 0; keys.size() < keyCount || keys.peek() + 1000 >= i; i++) {
-            String md5 = doMD5Hash(input + i, stretched);
+            String md5 = doMD5(input + i, stretched);
             int inARow = 0;
             char prev = ' ';
             int len = md5.length();
             boolean foundATriplet = false;
             for (int j = 0; j < len; j++) {
                 char c = md5.charAt(j);
-                if (c == prev) {
-                    ++inARow;
-                    if (!foundATriplet && inARow == 3) {
-                        tripletMap.computeIfAbsent(c, k -> new ArrayDeque<>()).add(i);
-                        foundATriplet = true;
-                    }
-                    if (inARow == 5) {
-                        Deque<Integer> indexes = tripletMap.get(c);
-                        if (indexes != null) {
-                            while (!indexes.isEmpty() && indexes.peek() + 1000 < i) indexes.poll();
-                            while (!indexes.isEmpty() && indexes.peek() < i) keys.add(indexes.poll());
-                            while (keys.size() > keyCount) keys.poll();
-                        }
-                    }
-                } else {
+                if (c != prev) {
                     inARow = 1;
                     prev = c;
+                    continue;
+                }
+                ++inARow;
+                if (!foundATriplet && inARow == 3) {
+                    tripletMap.computeIfAbsent(c, k -> new ArrayDeque<>()).add(i);
+                    foundATriplet = true;
+                }
+                if (inARow == 5) {
+                    Deque<Integer> indexes = tripletMap.get(c);
+                    if (indexes != null) {
+                        while (!indexes.isEmpty() && indexes.peek() + 1000 < i) indexes.poll();
+                        while (!indexes.isEmpty() && indexes.peek() < i) keys.add(indexes.poll());
+                        while (keys.size() > keyCount) keys.poll();
+                    }
                 }
             }
         }
         return keys.peek();
     }
 
-    private String doMD5Hash(String input, boolean stretch) throws Exception {
+    private String doMD5(String input, boolean stretch) throws Exception {
         String cur = input;
         for (int i = 0; i < (stretch ? 2017 : 1); i++) {
-            cur = doMD5(cur);
+            cur = Utils.doMD5(cur);
         }
         return cur;
     }
