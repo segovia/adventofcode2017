322f447ac7da0b503c61e07511fe34ed17e25e0a
Gustavo Segovia <gustavo.segovia@gmail.com>
2017-12-30 23:34:48 +0100

    D18 2016

---
+++ src/test/java/adventofcode/y2016/D18_LikeARogue.java
@@ -0,0 +1,43 @@
+package adventofcode.y2016;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class D18_LikeARogue {
+
+    @Test
+    public void test() throws Exception {
+        assertThat(run(".^^.^.^^^^", 10), is(38));
+        assertThat(run("^^.^..^.....^..^..^^...^^.^....^^^.^.^^....^.^^^...^^^^.^^^^.^..^^^^.^^.^.^.^.^.^^..." +
+                "^^..^^^..^.^^^^", 40), is(1961));
+        assertThat(run("^^.^..^.....^..^..^^...^^.^....^^^.^.^^....^.^^^...^^^^.^^^^.^..^^^^.^^.^.^.^.^.^^..." +
+                "^^..^^^..^.^^^^", 400000), is(20000795));
+
+    }
+
+    private int run(String input, int rows) throws Exception {
+        int count = 0;
+        int len = input.length();
+        boolean[] prevLine = new boolean[input.length()];
+        for (int i = 0; i < input.length(); i++) {
+            prevLine[i] = input.charAt(i) == '.';
+            if (prevLine[i]) ++count;
+        }
+
+        boolean[] nextLine = new boolean[prevLine.length];
+        for (int i = 1; i < rows; i++) {
+            for (int j = 0; j < len; j++) {
+                boolean left = j == 0 || prevLine[j - 1];
+                boolean right = j == len - 1 || prevLine[j + 1];
+                nextLine[j] = left == right;
+                if (nextLine[j]) ++count;
+            }
+            boolean[] aux = prevLine;
+            prevLine = nextLine;
+            nextLine = aux;
+        }
+        return count;
+    }
+}
