6585fc88f05d5ee9d8684aa1a17663d86c590c38
Gustavo Segovia <gustavo.segovia@ti8m.ch>
2018-01-04 07:19:44 +0100

    D01 2015

---
+++ src/test/java/adventofcode/y2015/D01_NotQuiteLisp_2.java
@@ -0,0 +1,30 @@
+package adventofcode.y2015;
+
+import adventofcode.Utils;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class D01_NotQuiteLisp_2 {
+
+    @Test
+    public void test() throws IOException {
+        assertThat(run(")"), is(1));
+        assertThat(run("()())"), is(5));
+        List<String> fileInputs = Utils.getInputsFromFiles(D01_NotQuiteLisp_2.class);
+        assertThat(run(fileInputs.get(0)), is(1795));
+    }
+
+    private int run(String input) {
+        int floor = 0;
+        for (int i = 0; i < input.length(); i++) {
+            floor += input.charAt(i) == '(' ? 1 : -1;
+            if (floor < 0) return i + 1;
+        }
+        return floor;
+    }
+}
