772af14e110973ca83c2789967f6846885992665
Gustavo Segovia <gustavo.segovia@ti8m.ch>
2018-01-08 09:20:39 +0100

    D12 2015

--- src/test/java/adventofcode/Utils.java
+++ src/test/java/adventofcode/Utils.java
@@ -74,4 +74,17 @@ public class Utils {
         byte[] bytesOfMessage = input.getBytes("UTF-8");
         return DatatypeConverter.printHexBinary(MD5.digest(bytesOfMessage)).toLowerCase();
     }
+
+    public static boolean isBlank(final CharSequence cs) {
+        int strLen;
+        if (cs == null || (strLen = cs.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(cs.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
